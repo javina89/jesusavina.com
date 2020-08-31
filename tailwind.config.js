82a13c94b0915edd9ce996c610b7793486977fcd
Jesus Avina <mail@jesusavina.com>
2020-08-31 00:48:59 -0500

    Added custom tailwin animation for theme icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
     theme: {
         extend: {
+          animation: {
+            'spin-ray': 'spin 20s linear infinite',
+            'swing-shadow': 'swing 20s linear infinite',
+          },
+          keyframes: {
+            swing: {
+              '0%, 100%': { transform: 'translate(-145%, -35%)' },
+              '50%': { transform: 'translate(-175%, -35%)' },
+            }
+          },            
           colors: {
             'bg-landing': '#2C2A37',
             'theme': 'var(--color-theme)',
