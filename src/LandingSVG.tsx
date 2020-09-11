9c85f3862a19331e72e01aafa74961d1fd437f28
Jesus Avina <mail@jesusavina.com>
2020-09-11 15:34:12 -0500

    testing svg size for mobile

--- src/LandingSVG.tsx
+++ src/LandingSVG.tsx
@@ -33,10 +33,10 @@ const LandingSVG = () => {
         }
     }
 
-    const sceneSVG = <motion.svg className="absolute inset-x-0 bottom-0" viewBox={`0 0 3338 2130`}><path id="NorthWater" d="M2.083,59.66c527.219,-47.696 1082.47,-68.249 1666.67,-20.037c562.792,-62.641 1118.12,-40.632 1666.67,20.037l-0,400.757l-3333.33,-0" fill="#61DAFB"/><rect id="Grass" x="2.083" y="460.417" width="3333.33" height="1000" fill="#4b9e00"/><g id="Road"><rect x="2.083" y="668.75" width="3333.33" height="458.333" fill="#2e2e2e"/><rect x="64.583" y="877.083" width="291.667" height="41.667" fill="#fff"/><rect x="647.917" y="877.083" width="291.667" height="41.667" fill="#fff"/><rect x="1231.25" y="877.083" width="291.667" height="41.667" fill="#fff"/><rect x="1814.58" y="877.083" width="291.667" height="41.667" fill="#fff"/><rect x="2397.92" y="877.083" width="291.667" height="41.667" fill="#fff"/><rect x="2981.25" y="877.083" width="291.667" height="41.667" fill="#fff"/></g><rect id="Sand" x="2.083" y="1460.42" width="3333.33" height="333.333" fill="#e5ac7c"/><path id="SouthWater" d={`M2.083,1726.33c527.219,-47.696 1082.47,-68.249 1666.67,-20.038c562.792,-62.641 1118.12,-40.631 1666.67,20.038l-0,400.756l-3333.33,0`} fill="#61DAFB"/></motion.svg>
+    const sceneSVG = <motion.svg className="absolute inset-x-0 bottom-0" viewBox="0 0 3339 3311"><path id="NorthWater" d="M2.406,79.175c527.218,-63.595 1082.47,-90.999 1666.67,-26.717c562.792,-83.521 1118.12,-54.175 1666.67,26.717l-0,534.342l-3333.33,-0" fill="#01f"/><rect id="Grass" x="2.406" y="613.517" width="3333.33" height="1805.56" fill="#4b9e00"/><g id="Road"><rect x="2.406" y="1113.52" width="3333.33" height="611.111" fill="#2e2e2e"/><rect x="64.906" y="1391.3" width="291.667" height="55.556" fill="#fff"/><rect x="648.239" y="1391.3" width="291.667" height="55.556" fill="#fff"/><rect x="1231.57" y="1391.3" width="291.667" height="55.556" fill="#fff"/><rect x="1814.91" y="1391.3" width="291.667" height="55.556" fill="#fff"/><rect x="2398.24" y="1391.3" width="291.667" height="55.556" fill="#fff"/><rect x="2981.57" y="1391.3" width="291.667" height="55.556" fill="#fff"/></g><rect id="Sand" x="2.406" y="2196.85" width="3333.33" height="666.667" fill="#e5ac7c"/><path id="SouthWater" d="M2.406,2773.62c527.218,-63.595 1082.47,-90.998 1666.67,-26.717c562.792,-83.521 1118.12,-54.174 1666.67,26.717l-0,534.342l-3333.33,0" fill="#01f"/></motion.svg>
 
     return (
-        <div className="relative max-w-5xl">
+        <div className="max-w-xs">
             {sceneSVG}
         </div>
         )
